3a1d423a0466ca8adc5d6c5beb4aa906d41f6975
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-11 13:54:49 -0600

    📅  | Mon Apr 11 13:54:49 MDT 2022

--- app-gateway/src/app.module.ts
+++ app-gateway/src/app.module.ts
@@ -2,10 +2,12 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
   imports: [
-    ClientsModule.register([{ name: 'CHAT', transport: Transport.TCP }]),
+    ClientsModule.register([
+      { name: 'CHAT', transport: Transport.TCP },
+      { name: 'AUTH', transport: Transport.TCP, options: { port: 3001 } },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
